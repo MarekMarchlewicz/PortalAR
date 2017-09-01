ef2d6fdb65fe902e2bb4dc669143125ab06c475b
Marek <marek.marchlewicz@gmail.com>
2017-09-01 14:29:08 +0800

    added drag planes facing camera

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraTransform;
+
+    private Transform m_Transform;
+
+    private void Awake()
+    {
+        m_Transform = GetComponent<Transform>();
+    }
+
+	private void Update()
+    {
+        if(cameraTransform == null)
+        {
+            cameraTransform = Camera.main.transform;
+        }
+
+        Vector3 newOrientation = Quaternion.LookRotation(cameraTransform.position - m_Transform.position).eulerAngles;
+        newOrientation.x = 0f;
+        newOrientation.z = 0f;
+
+        m_Transform.rotation = Quaternion.Euler(newOrientation);
+    }
+}
